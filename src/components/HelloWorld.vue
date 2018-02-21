4bf52bae182096b8f17bb3ddfe1ede3d5799a6cd
Raphael BAUM <rbaum@e1r4p8.42.fr>
2018-02-21 16:57:49 +0100

    Ugly prototype

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,74 +1,93 @@
-//TODO
-/*
-	Parse file, get knex stuff, render it properly
-*/
-
 <template>
-<div class="hello">
+<v-container fluid>
+  <h4>Example: postgresql use : pg_dump -s databasename > file.sql</h4>
+  <h4>Example:  sql use mysqldump -u root -p --no-data dbname > schema.sql</h4>
+  <h5> Then use that file</h5>
   <input type="file"
     multiple
     @change='read' />
-  <v-layout v-model="test">
-    <v-flex v-for="(value, key) in test"
-      :key='key'>
-      <v-card v-model="test">
-        <v-card-title primary-title>{{key}}</v-card-title>
-        <v-flex v-for="(v,k) in value"
-          :key="k">
-          <v-card-text>
-            {{v}} </v-card-text>
-        </v-flex>
-        <v-card-text></v-card-text>
-      </v-card>
-    </v-flex>
+  <v-layout wrap
+    v-model="tables">
+    <transition-group name="list"
+      class="rs-flex">
+      <v-flex v-for="(value, key) in tables"
+        :key='key'>
+        <Table :tKey="key"
+          :color="colors[key]"
+          :colors="colors"
+          :tValue="value"> </Table>
+      </v-flex>
+    </transition-group>
   </v-layout>
-</div>
+</v-container>
 </template>
 
 <script>
+import Table from './Table';
 export default {
   name: 'HelloWorld',
+  components: {
+    Table
+  },
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
-      test: {},
+      tables: {},
+      references: {},
+      colors: {},
       text: []
     }
   },
+  computed: {},
   methods: {
-    parse(str) {
-      const regex = /createTable.[^\(]+\((.[^,]*),.[^\(]*\(.[^\{]*\{(.[^\}]*)\}/g;
-      const subRegex = /(?=(increments|string|boolean|timestamp|integer|foreign)\(['|"](\w+))/g;
+    colorGen: function() {
+      let color = []
+      color[0] = Math.random() * Math.floor(360);
+      color[1] = `${Math.random() * Math.floor(80) + 20}%`;
+      color[2] = `${Math.random() * Math.floor(80) }%`;
+      return `hsl(${color[0]}, ${color[1]}, ${color[2]})`
+
+    },
+
+    // ******** ********  parseKnex  ******** ********
+    parseKnex(str) {
+      //TODO : better regex, also for .sql type
+      const regex = /CREATE TABLE `?(\w+)`? ?∆(.[^Ω]*)Ω/g;
+      const subRegex = /FOREIGN KEY \([`]?(\w+)[`]?\) REFERENCES (.[^(]+)/g;
       let m, n;
       while ((m = regex.exec(str)) !== null) {
         if (m.index === regex.lastIndex) {
           regex.lastIndex++;
         }
-        let k = [];
-        while ((n = subRegex.exec(m[2])) !== null) {
-          if (n.index === subRegex.lastIndex) {
-            subRegex.lastIndex++;
-          }
-          k.push({
-            [n[1]]: n[2]
-          });
-        }
-        console.log('k', k)
-        this.$set(this.test, m[1], k);
-        // this.test[m[1]] = k;
+        let z = m[2].split(',').map(e => e.trim()).filter(e => !(e.startsWith('CONSTRAINT') || e.startsWith('KEY') || e.startsWith('PRIMARY')));
+        let zz = {};
+        z.map(e => {
+          let x = e.split(' ');
+          zz[[x[0]]] = x[1];
+        })
+        this.$set(this.tables, m[1], zz);
+        this.$set(this.colors, m[1], this.colorGen())
       }
+      while ((n = subRegex.exec(str)) !== null) {
+        if (n.index === subRegex.lastIndex) {
+          subRegex.lastIndex++;
+        }
+        this.references[[n[2]]] = n[1];
+      }
+
     },
+    // ******** ********  Read  ******** ********
     read(data) {
       let files = data.target.files;
       for (let i = 0, file; file = files[i]; i++) {
         let reader = new FileReader();
         reader.onload = e => {
-          this.text.push(e.target.result.replace(/\n|\r/g, ""));
-          this.parse(this.text[0]);
-          console.log('-------------')
+          this.text.push(e.target.result.replace(/\(\s/g, "∆").replace(/\)(;| E)/g, "Ω").replace(/\n|\r/g, ""));
+          this.parseKnex(this.text[i]);
         };
         reader.readAsText(file);
       }
+      console.log('text', this.text)
     }
   }
 }
@@ -76,23 +95,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.rs-flex {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
 
@@ -117,4 +125,11 @@ createTable.[^\(]+\((.[^,]*),.[^\(]*\(.[^\{]*\{(.[^\}]*)\}
 (?=increments|string|boolean|timestamp).[^\(]*\(['|"](\w+)['|"], *(\d*)
 -------------
 /(?=(increments|string|boolean|timestamp)\(['|"](\w+))/g
+
+sql
+
+FOREIGN KEY \([`]?(\w+)[`]?\) REFERENCES (.[^(]+)
+
+
+
 */
---
+++ src/components/Table.vue
@@ -0,0 +1,46 @@
+  <template>
+	<v-card class="drawing"
+	:style="{borderTop: '6px solid', borderColor: color}" >
+		<v-card-title primary-title><h1>{{tKey}}</h1></v-card-title>
+		<v-flex v-for="(v,k) in tValue"
+			:key="k">
+			<v-card-text>
+				<h5>{{k}}</h5> <span :style="{color: v == 'foreign' ? 'red' : 'hsl(249,9%,80%)'}">{{v}}</span>
+ </v-card-text>
+		</v-flex>
+		<v-card-text></v-card-text>
+	</v-card>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: ['tKey', 'tValue', 'color', 'colors'],
+
+  data: function() {
+    return {}
+  },
+  computed: {},
+  methods: {
+    colorGen: function() {
+      let color = []
+      color[0] = Math.random() * Math.floor(360);
+      color[1] = `${Math.random() * Math.floor(80) + 20}%`;
+      color[2] = `${Math.random() * Math.floor(80) }%`;
+      // let color = "#" + ((1 << 24) * Math.random() | 0).toString(16)
+      return {
+        borderTop: "5px solid",
+        borderColor: `hsl(${color[0]}, ${color[1]}, ${color[2]})`
+      }
+    },
+  },
+  components: {},
+}
+</script>
+
+<style scoped>
+.drawing {
+  margin: 2vh;
+  padding: 2vh;
+}
+</style>
